8f4525626f1f9c060a1de001e5c138d98b627171
JordonBruton <jrb123@humbolt.edu>
2021-11-29 16:32:16 -0800

    Fixing compilation errors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 
-const CONNECTION_URL = '';
+const CONNECTION_URL = 'mongodb+srv://JordonBruton:Rae25611!@cluster0.6nmvg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
